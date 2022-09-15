b052e01cf2a7d7e3039a4951a6c5443c9c5325d9
Nevyan Plamenov <plamenovnevyan@gmail.com>
2022-09-15 15:15:58 +0300

    Changed async email sending

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -2,13 +2,12 @@ const router = require('express').Router()
 const emailServices = require('../services/emailServices')
 
 router.post('/send', async (req, res) => {
-   try{
-    await emailServices.checkIfEmailExists(req.body.email)
-    emailServices.sendEmail(req.body)
-    res.json({message : 'Success'})
-   }catch(err){
-    res.status(400).json({message : err.message})
-   }
+    emailServices.checkIfEmailExists(req.body.email)
+    .then(() => {
+      emailServices.sendEmail(req.body)
+      res.json({message : 'Success'})
+    })
+   .catch(err =>  res.status(400).json({message : err.message})) 
 })
 
 module.exports = router

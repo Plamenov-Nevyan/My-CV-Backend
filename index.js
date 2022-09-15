9da12eff9b9d21a505ea243023c9e92ecaaa075f
Nevyan Plamenov <plamenovnevyan@gmail.com>
2022-09-15 14:09:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const routes = require('./routes')
 const cors = require('./middlewares/cors')
 const dbConnect = require('./config/mongoDB')
-const port = process.env.PORT || 5000
+const port = process.env.PORT 
 
 
 app.use(cors)
